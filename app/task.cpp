1bd54e2f4b137dc898ee135bf133b80a228dcebe
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2024-12-01 17:29:00 -0300

    chore: estrutura para lista de tarefas

---
+++ app/task.cpp
@@ -0,0 +1,40 @@
+#include "./task.h"
+
+TaskList::TaskList() {
+    size = 0;
+}
+
+void TaskList::push(char* buffer) {
+    Element data; 
+    data = new Task;
+
+    data->value = buffer;
+    data->next = nullptr;
+
+    if(size == 0) {
+        start = data; 
+        end = data;
+    } else {
+        end->next = data;
+        end = data;
+    }
+
+    size +=1;
+}
+
+char* TaskList::pop() {
+    char *buffer;
+    Element aux; 
+
+    if(size == 0) return buffer;
+
+    buffer = start->value;
+    aux = start;
+
+    start = start->next; 
+    delete aux;
+
+    size -=1;
+
+    return buffer;
+}
